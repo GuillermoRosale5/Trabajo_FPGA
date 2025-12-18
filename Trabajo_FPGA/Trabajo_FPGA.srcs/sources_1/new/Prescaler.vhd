2de47892dc6777865c3904fdd24fd9e350a39719
lorenzohidalgoanero <lorenzo.hidalgo@alumnos.upm.es>
2025-12-18 12:13:42 +0100

    Update Prescaler.vhd

--- Trabajo_FPGA/Trabajo_FPGA.srcs/sources_1/new/Prescaler.vhd
+++ Trabajo_FPGA/Trabajo_FPGA.srcs/sources_1/new/Prescaler.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 09.12.2025 19:33:09
+-- Create Date: 11.12.2025 17:03:30
 -- Design Name: 
 -- Module Name: Prescaler - Behavioral
 -- Project Name: 
@@ -21,23 +21,41 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use ieee.numeric_std.all;
 
 entity Prescaler is
---  Port ( );
+    port(
+        clk_in  : in  std_logic;   -- 100 MHz
+        rst     : in  std_logic;   -- Reset síncrono
+        clk_out : out std_logic    -- 16 kHz
+    );
 end Prescaler;
 
 architecture Behavioral of Prescaler is
 
+    constant DIV_COUNT : integer := 3124;  -- (100M/16k)/2 - 1
+    signal counter     : integer range 0 to DIV_COUNT := 0;
+    signal clk_reg     : std_logic := '0';
+
 begin
 
+    process(clk_in)
+    begin
+        if rising_edge(clk_in) then
+            if rst = '0' then
+                counter <= 0;
+                clk_reg <= '0';
+            else
+                if counter = DIV_COUNT then
+                    counter <= 0;
+                    clk_reg <= not clk_reg; -- toggle del divisor
+                else
+                    counter <= counter + 1;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    clk_out <= clk_reg;
 
 end Behavioral;
